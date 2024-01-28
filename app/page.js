1b12776b404dcd6a3dfb05bd93772f0a207fb07f
Debasish Raut <debasishraut1996@outlook.com>
2024-01-28 21:01:51 +0530

    added particles animation

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import IntroContent from "@/components/IntroContent/IntroContent";
+import Particle from "@/components/Particles/Particles";
 
 export default function Home() {
   return (
---
+++ components/Particles/Particles.js
@@ -0,0 +1,103 @@
+"use client";
+import { useCallback } from "react";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+export default function Particle() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      canvasClassName="particles"
+      height={520}
+      width={1480}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "transparent",
+          },
+        },
+        fullScreen: {
+          enable: false,
+          zIndex: -1, // or any value is good for you, if you use -1 set `interactivity.detectsOn` to `"window"` if you need mouse interactions
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "grab",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 12,
+            },
+            grab: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 100,
+            enable: true,
+            opacity: 1,
+            width: 1,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1500,
+            },
+            value: 200,
+          },
+          opacity: {
+            value: 1,
+          },
+          shape: {
+            type: "square",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: false,
+      }}
+    />
+  );
+}
